711f99a7e48cafd02ae9ac6b947096adc5c5cf19
moracicero <135073675+moracicero@users.noreply.github.com>
2025-06-23 20:47:27 -0300

    Add files via upload

---
+++ json-data/peliculasyseries.js
@@ -0,0 +1,442 @@
+const DATA_PELICULAS = [
+    {
+        "id": "the-incredible-hulk", // Added for easier mapping to detail pages
+        "imageUrl": "assets/hulk_movie.jpg",
+        "heroimg": "assets/hulk_hero.png",
+        "title": "The incredible Hulk",
+        "clasificacion": 13,
+        "año": 2008,
+        "genre": [
+            "Acción",
+            "Fantasía"
+        ],
+        "descripcion": "En The Incredible Hulk, Bruce Banner se convierte en Hulk como un peón involuntario en un esquema militar para revitalizar el programa del supersoldado a través de radiación gamma.",
+        "resumen": "Bruce Banner recorre el mundo en busca de un antídoto para librarse de su alter ego. Además tendrá que hacer frente a Emil, un nuevo enemigo, lo que convertirá a Nueva York en el escenario de la última batalla entre las dos criaturas más poderosas.",
+        "trailer": "https://www.youtube.com/watch?v=xbqNb2PFKKA",
+        "iframe": "https://www.youtube.com/embed/xbqNb2PFKKA?si=5rnsgDP6cUfOwseY",
+        "elenco": [
+            "Edward Norton",
+            "Liv Tyler",
+            "Tim Roth",
+            "William Hurt",
+            "Tim Blake Nelson",
+            "Ty Burrell",
+            "Christina Cabot",
+            "Peter Mensah"
+        ],
+        "directores": [
+            "Louis Leterrier"
+        ],
+        "productores": [
+            "Avi Arad",
+            "Kevin Feige",
+            "Gale Anne Hurd",
+            "Stan Lee",
+            "David Maisel",
+            "Jim Van Wyck"
+        ],
+        "temporadas": 0,
+        "capitulos": 0
+    },
+    {
+        "id": "supercool", // Added
+        "imageUrl": "assets/supercool_movie.png",
+        "heroimg": "assets/super-cool_hero.png",
+        "title": "Supercool",
+        "clasificacion": 18,
+        "año": 2007,
+        "genre": [
+            "Comedia",
+            "Adolescente"
+        ],
+        "descripcion": "Un adolescente intenta navegar por las complejidades de la vida, el amor y la amistad mientras enfrenta los desafíos de crecer.",
+        "resumen": "La historia sigue a un grupo de amigos mientras lidian con los altibajos de la adolescencia, explorando temas de identidad, pertenencia y el viaje hacia la adultez.",
+        "trailer": "https://www.youtube.com/watch?v=au2Zq8D9RaY",
+        "iframe": "https://www.youtube.com/embed/au2Zq8D9RaY?si=uD-8ST23VjoB2dCU",
+        "elenco": [
+            "Michael Cera",
+            "Christopher Mintz-Plasse",
+            "Jonah Hill",
+            "Emma Stone",
+        ],
+        "directores": [
+            "Greg Mottola"
+        ],
+        "productores": [
+            "Evan Goldberg",
+            "Greg Mottola",
+        ],
+        "temporadas": 0,
+        "capitulos": 0
+    },
+    {
+        "id": "baila-vini", // Added
+        "imageUrl": "assets/baila-vini_movie.jpg",
+        "heroimg": "assets/baila-vini_hero.jpg",
+        "title": "Baila, Vini",
+        "clasificacion": 0,
+        "año": 2025,
+        "genre": [
+            "Documental"
+        ],
+        "descripcion": "Narra la vida y carrera de Vinícius Júnior, el futbolista brasileño, desde sus inicios hasta su consagración como una estrella mundial.",
+        "resumen": "Baila, Vini es un documental de Netflix sobre el futbolista brasileño Vinícius Júnior",
+        "trailer": "https://www.youtube.com/watch?v=IucwRVDHroc",
+        "iframe": "https://www.youtube.com/embed/IucwRVDHroc?si=5Hydl02FM8oIOmxS",
+        "elenco": [
+            "Vinicius Júnior",
+            "Neymar Jr.",
+            "Thiago Silva",
+            "Gabriel Jesus"
+        ],
+        "directores": [
+            "Andrucha Waddington",
+            "Emilio Domingos"
+        ],
+        "productores": [
+            "Luisa Barbosa",
+            "Renata Brandão"
+        ],
+        "temporadas": 0,
+        "capitulos": 0
+    },
+    {
+        "id": "shaolin-soccer", // Added
+        "imageUrl": "assets/shaolin-soccer_movie.png",
+        "heroimg": "assets/shaolin-soccer_hero.png",
+        "title": "Shaolin soccer",
+        "clasificacion": 13,
+        "año": 2001,
+        "genre": [
+            "Acción",
+            "Comedia"
+        ],
+        "descripcion": "Sing es alumno del Shaolin y su verdadera ambición es popularizar y aproximar el arte del kung fu a la sociedad moderna.",
+        "resumen": "Un día conoce a Fung, un antiguo futbolista fracasado, ambos establecerán una buena relación de la que nacerá la idea de formar un equipo que combine el futbol con las artes marciales.",
+        "trailer": "https://www.youtube.com/watch?v=KEWUDWYDdYs",
+        "iframe": "https://www.youtube.com/embed/KEWUDWYDdYs?si=P2JutV9XXuDTRntV",
+        "elenco": [
+            "Stephen Chow",
+            "Wei Zhao",
+            "Wong yat fei",
+            "Karen Mok",
+            "Cecilia Cheung",
+            "Danny Chan Kwok Kwan",
+            "Chiu Chi Ling",
+            "Zhang Ming Ming"
+        ],
+        "directores": [
+            "Stephen Chow"
+        ],
+        "productores": [
+            "Yeung Kwok Fai"
+        ],
+        "temporadas": 0,
+        "capitulos": 0
+    },
+    {
+        "id": "bad-boys-for-life", // Added
+        "imageUrl": "assets/bad-boys_movie.jpg",
+        "heroimg": "assets/bad-boys_hero.jpg",
+        "title": "Bad Boys For Life",
+        "clasificacion": 17,
+        "año": 2020,
+        "genre": [
+            "Acción",
+            "Comedia"
+        ],
+        "descripcion": "Los detectives de Miami, Mike Lowrey y Marcus Burnett, se reúnen una última vez para acabar con un cartel de drogas que amenaza sus vidas.",
+        "resumen": "Mike y Marcus se ven obligados a trabajar juntos una vez más cuando un nuevo enemigo aparece en la escena, poniendo en peligro todo lo que han construido.",
+        "trailer": "https://www.youtube.com/watch?v=enizDUYZOCI",
+        "iframe": "https://www.youtube.com/embed/enizDUYZOCI?si=4nXW1Q5GUX1iktjK",
+        "elenco": [
+            "Will Smith",
+            "Martin Lawrence",
+            "Vanessa Hudgens",
+            "Alexander Ludwig",
+            "Charles Melton",
+            "Paola Núñez",
+            "Nicky Jam",
+            "Kate del Castillo"
+        ],
+        "directores": [
+            "Adil El Arbi",
+            "Bilall Fallah"
+        ],
+        "productores": [
+            "Jerry Bruckheimer",
+            "Will Smith",
+            "Doug Belgrad",
+            "Chad Oman",
+            "Mike Stenson",
+            "James Lassiter",
+            "Barry H. Waldman"
+        ],
+        "temporadas": 0,
+        "capitulos": 0
+    },
+    {
+        "id": "el-juego-del-calamar", // Added
+        "imageUrl": "assets/squid-game_series.jpg",
+        "heroimg": "assets/squid-game_hero.jpg",
+        "title": "El juego del calamar",
+        "clasificacion": 16,
+        "año": 2021,
+        "genre": [
+            "Drama",
+            "Suspenso"
+        ],
+        "descripcion": "Un grupo de personas con problemas financieros acepta una misteriosa invitación para participar en un juego de supervivencia con un premio en efectivo.",
+        "resumen": "A medida que los jugadores compiten en juegos infantiles con consecuencias mortales, deben enfrentarse a sus propios demonios y a la naturaleza humana.",
+        "trailer": "https://www.youtube.com/watch?v=oqxAJKy0ii4",
+        "iframe": "https://www.youtube.com/embed/oqxAJKy0ii4?si=W8Sa-153t-5EAXdh",
+        "elenco": [
+            "Jung-jae Lee",
+            "Lee Byung-hun",
+            "Wi Ha-Joon",
+            "Ha-Neul Kang",
+            "Sung-Hoon Park",
+            "Yu-ri Jo",
+            "Si-wan Yim",
+            "Yang Dong-Geun",
+            "Ae-sim Kang",
+            "Jin-wook Lee",
+            "Jae-won Roh",
+            "Ji-an Won",
+            "Park Gyu-young",
+            "Dai-Su Oh"
+        ],
+        "directores": [
+            "Hwang Dong-hyuk"
+        ],
+        "productores": [
+            "Hwang Dong-hyuk",
+            "Kim Ji-yeon"
+        ],
+        "temporadas": [
+            "Temporada 1",
+            "Temporada 2",
+            "Temporada 3"
+        ],
+        "capitulos": [
+            "Capitulo 1",
+            "Capitulo 2",
+            "Capitulo 3",
+            "Capitulo 4",
+            "Capitulo 5",
+            "Capitulo 6",
+            "Capitulo 7",
+            "Capitulo 8",
+            "Capitulo 9",
+            "... 17"
+        ]
+    },
+    {
+        "id": "death-note", // Added
+        "imageUrl": "assets/death-note_series.jpg",
+        "heroimg": "assets/death-note_hero.jpg",
+        "title": "Death note",
+        "clasificacion": 14,
+        "año": 2006,
+        "genre": [
+            "Suspenso",
+            "Fantasía"
+        ],
+        "descripcion": "Un libro malévolo cae en manos de Yagami Light. Ese libro tiene unas instrucciones claras y encierra una magia oscura.",
+        "resumen": "Yagami no lo cree en un principio pero una vez lo prueba y comprueba que es cierto, toda su vida cambia por completo. El protagonista usará este artilugio para eliminar del mundo todos esas personas malas, criminales y seres oscuros y malvados que lo acechan.",
+        "trailer": "https://www.youtube.com/watch?v=NlJZ-YgAt-c",
+        "iframe": "https://www.youtube.com/embed/NlJZ-YgAt-c?si=QT0f0Ty4FVurOdq5",
+        "elenco": [
+            "Mamoru Miyano",
+            "Shido Nakamura",
+            "Noriko Hidaka",
+            "Kappei Yamaguchi",
+            "Aya Hirano",
+            "Nozomu Sasaki",
+            "Naoya Uchida"
+        ],
+        "directores": [
+            "Tetsurō Araki"
+        ],
+        "productores": [
+            ""
+        ],
+        "temporadas": [
+            "Temporada 1"
+        ],
+        "capitulos": [
+            "Capitulo 1",
+            "Capitulo 2",
+            "Capitulo 3",
+            "Capitulo 4",
+            "Capitulo 5",
+            "Capitulo 6",
+            "Capitulo 7",
+            "Capitulo 8",
+            "Capitulo 9",
+            "... 37"
+        ]
+    },
+    {
+        "id": "breaking-bad", // Added
+        "imageUrl": "assets/breaking-bad_series.jpg",
+        "heroimg": "assets/breaking-bad_hero.jpg",
+        "title": "Breaking Bad",
+        "clasificacion": 14,
+        "año": 2008,
+        "genre": [
+            "Drama"
+        ],
+        "descripcion": "Un profesor de química se convierte en fabricante de metanfetaminas para asegurar el futuro financiero de su familia.",
+        "resumen": "Walter White, un profesor de química, se asocia con un exalumno para producir y vender metanfetaminas tras ser diagnosticado con cáncer.",
+        "trailer": "https://www.youtube.com/watch?v=HhesaQXLuRY",
+        "iframe": "https://www.youtube.com/embed/HhesaQXLuRY?si=TIbyVr-xCL4qO-6o",
+        "elenco": [
+            "Bryan Cranston",
+            "Aaron Paul",
+            "Anna Gunn",
+            "Dean Norris",
+            "Betsy Brandt",
+            "RJ Mitte",
+            "Bob Odenkirk",
+            "Jonathan Banks"
+        ],
+        "directores": [
+            "Vince Gilligan",
+            "Adam Bernstein",
+            "Michelle MacLaren",
+            "Michael Slovis",
+            "Peter Gould",
+            "Colin Bucksey",
+            "Thomas Schnauz",
+            "Sam Catlin",
+            "Rian Johnson",
+            "Bryan Cranston"
+        ],
+        "productores": [
+            "Karen Moore"
+        ],
+        "temporadas": [
+            "Temporada 1",
+            "Temporada 2",
+            "Temporada 3",
+            "Temporada 4",
+            "Temporada 5",
+        ],
+        "capitulos": [
+            "Capitulo 1",
+            "Capitulo 2",
+            "Capitulo 3",
+            "Capitulo 4",
+            "Capitulo 5",
+            "Capitulo 6",
+            "Capitulo 7",
+            "Capitulo 8",
+            "Capitulo 9",
+            "... 62"
+        ]
+    },
+    {
+        "id": "el-eternauta", // Added
+        "imageUrl": "assets/eternauta_series.jpg",
+        "heroimg": "assets/eternauta_hero.jpg",
+        "title": "El eternauta",
+        "clasificacion": 14,
+        "año": 2025,
+        "genre": [
+            "Drama",
+            "Ciencia Ficción"
+        ],
+        "descripcion": "Un grupo de amigos se enfrenta a una invasión alienígena mientras intentan sobrevivir en un mundo post-apocalíptico.",
+        "resumen": "El eternauta es una historia de ciencia ficción que explora la lucha de la humanidad contra fuerzas externas y la importancia de la amistad y la solidaridad.",
+        "trailer": "https://www.youtube.com/watch?v=ykLTd5aTa88",
+        "iframe": "https://www.youtube.com/embed/ykLTd5aTa88?si=k3ofAxCLHR7CcXHh",
+        "elenco": [
+            "Ricardo Darín",
+            "Carla Peterson",
+            "Cesar Troncoso",
+            "Andrea Pietra",
+            "Ariel Staltari",
+            "Marcelo Subiotto",
+            "Claudio Martínez Bel",
+            "Orianna Cardenas",
+            "Mora Fisz"
+        ],
+        "directores": [
+            "Bruno Stagnaro"
+        ],
+        "productores": [
+            "Micaela Buye",
+            "Hugo Sigman",
+            "Matias Mosteirín",
+            "Leticia Cristi",
+            "Diego Copello"
+        ],
+        "temporadas": [
+            "Temporada 1"
+        ],
+        "capitulos": [
+            "Capitulo 1",
+            "Capitulo 2",
+            "Capitulo 3",
+            "Capitulo 4",
+            "Capitulo 5",
+            "Capitulo 6"
+        ]
+    },
+    {
+        "id": "the-office", // Added
+        "imageUrl": "assets/the-office_series.jpg",
+        "heroimg": "assets/the-office_hero.jpg",
+        "title": "The Office",
+        "clasificacion": 13,
+        "año": 2005,
+        "genre": [
+            "Comedia"
+        ],
+        "descripcion": "The Office es una serie de televisión de comedia que sigue el día a día de los empleados de una sucursal de la empresa papelera ficticia",
+        "resumen": "The Office , popular serie de comedia de situación estadounidense que narra la vida cotidiana de un grupo de empleados de la ficticia empresa Dunder Mifflin Paper Company en Scranton , Pensilvania",
+        "trailer": "https://www.youtube.com/watch?v=vyq1FCeFrng",
+        "iframe": "https://www.youtube.com/embed/vyq1FCeFrng?si=ie9e1jLa7AX0mRPW",
+        "elenco": [
+            "Steve Carell",
+            "Rainn Wilson",
+            "John Krasinski",
+            "Jenna Fischer",
+            "B. J. Novak"
+        ],
+        "directores": [
+            "Amy Heckerling",
+            "Greg Daniels",
+            "Bryan Gordon",
+            "Ken Kwapis",
+            "Ken Whittingham"
+        ],
+        "productores": [
+            ""
+        ],
+        "temporadas": [
+            "Temporada 1",
+            "Temporada 2",
+            "Temporada 3",
+            "Temporada 4",
+            "Temporada 5",
+            "Temporada 6",
+            "Temporada 7",
+            "Temporada 8",
+            "Temporada 9"
+        ],
+        "capitulos": [
+            "Capitulo 1",
+            "Capitulo 2",
+            "Capitulo 3",
+            "Capitulo 4",
+            "Capitulo 5",
+            "Capitulo 6",
+            "Capitulo 7",
+            "Capitulo 8",
+            "Capitulo 9",
+            "... 201"
+        ]
+    }
+];
