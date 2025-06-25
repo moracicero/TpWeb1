864cd141f0ef049373f1f70a80c6b481cb839995
Ani <73732038+ana-maria25@users.noreply.github.com>
2025-06-25 00:15:27 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const usuarioInput = document.getElementById("nombre");
   const contraseniaInput = document.getElementById("contrasenia");
   const iniciarBtn = document.getElementById("btn-iniciar");
-  const errorMsg = document.createElement("p");
+  const errorMsg = document.createElement("p"); //	Crea un párrafo vacío
 
   errorMsg.style.color = "red";
   errorMsg.style.fontSize = "0.9rem";
   errorMsg.style.textAlign = "center";
 
   const form = document.querySelector(".login-form");
-  form.appendChild(errorMsg);
+  form.appendChild(errorMsg); //Lo pone debajo del formulario
 
   const actualizarEstadoBoton = () => {
     iniciarBtn.disabled = !(
@@ -17,25 +17,32 @@ document.addEventListener("DOMContentLoaded", function () {
     );
   };
 
+  //Sirve para escuchar eventos (como clics, escritura en campos, carga de página).Cuando el usuario escribe en el campo, se ejecuta una función.
   usuarioInput.addEventListener("input", actualizarEstadoBoton);
   contraseniaInput.addEventListener("input", actualizarEstadoBoton);
 
   iniciarBtn.addEventListener("click", function (e) {
     e.preventDefault();
 
+    // Traemos del localStorage una lista de usuarios guardados como texto, y la convertimos a un array para poder usarla.
     const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-    const usuario = usuarioInput.value.trim();
+    const usuario = usuarioInput.value.trim(); //.trim() Quita los espacios al principio y final de un texto.
     const pass = contraseniaInput.value;
 
+    //Busca si existe un usuario con esos datos.
+    // u representa..Cada elemento del array usuarios
     const encontrado = usuarios.find(
       (u) => u.usuario === usuario && u.contrasenia === pass
     );
 
     if (encontrado) {
-      errorMsg.textContent = "";
+      //al estar regristrado
+      errorMsg.textContent = ""; //.textContent Cambia el texto que se ve en un elemento HTML.
+      localStorage.setItem("usuario", JSON.stringify(encontrado)); //guarda usuario en inic sesi
       alert("¡Inicio de sesión exitoso!");
-      window.location.href = "home.html"; // Cambia el nombre si tu página es otra
+      window.location.href = "home.html";
     } else {
+      //si no esta registrado,le pone el texto cuando hay un error
       errorMsg.textContent = "Usuario o contraseña incorrectos.";
     }
   });
@@ -43,3 +50,4 @@ document.addEventListener("DOMContentLoaded", function () {
   // Desactivar el botón al cargar
   iniciarBtn.disabled = true;
 });
+
